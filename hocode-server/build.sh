3dbb770ab7d489b5818eae0656604a8e4c66557f
Neun <duythien0912@gmail.com>
2019-11-14 11:55:17 +0700

    deploy to vps

--- hocode-server/build.sh
+++ hocode-server/build.sh
@@ -6,4 +6,4 @@ git commit -m "deploy to vps"
 
 git push -u origin master
 
-ssh root@45.76.179.78 cd hocode && git pull && cd hocode-server && go build && sudo systemctl restart hocode
+ssh root@45.76.179.78 "source ~/.profile && cd hocode && git pull && cd hocode-server && go build && sudo systemctl restart hocode"
