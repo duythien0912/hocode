b0a14092d9cba6e4f23df0d052ad6eb121b3760a
Neun <duythien0912@gmail.com>
2019-11-16 16:57:50 +0700

    [Thien] deploy to vps

--- hocode-server/server.go
+++ hocode-server/server.go
@@ -20,7 +20,7 @@ import (
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host hocode.appspot.com
+// @host hocodevn.com
 // @BasePath /api/v1
 
 func main() {
