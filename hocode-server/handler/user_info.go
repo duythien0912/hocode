28e0734d4d6b49f14219e334129b9f0c2d6e504a
Neun <duythien0912@gmail.com>
2019-11-16 11:40:29 +0700

    [Thien] deploy to vps

--- hocode-server/handler/user_info.go
+++ hocode-server/handler/user_info.go
@@ -39,6 +39,7 @@ func (h *Handler) GetUserCourse(c echo.Context) (err error) {
 		One(&uc); err != nil {
 		if err == mgo.ErrNotFound {
 			// return echo.ErrNotFound
+			uc.CourseInfo = []model.CourseInfo{}
 			return c.JSON(http.StatusOK, uc)
 		}
 
