810a4f85059c592956c6c9a296344ef7cf0213f1
Neun <duythien0912@gmail.com>
2019-11-20 11:16:16 +0700

    [Thien] deploy to vps

--- hocode-server/config/config.go
+++ hocode-server/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 
-
+// mongodb://admin:adminadmin1@ds021984.mlab.com:21984/hocode
+// mongodb://admin:adminadmin1@ds335668.mlab.com:35668/hocode3
 const(
-	NameDb string = "hocode3"
-	LinkDb string = "mongodb://admin:adminadmin1@ds335668.mlab.com:35668/"
+	NameDb string = "hocode"
+	LinkDb string = "mongodb://admin:adminadmin1@ds021984.mlab.com:21984/"
 )
