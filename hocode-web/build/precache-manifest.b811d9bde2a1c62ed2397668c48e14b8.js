80b35869469521d92af851488f1a657c02deb430
Lê Hoàng Giang <gianglh4j@gmail.com>
2019-11-22 13:33:48 +0700

    [Giang] fix header minitask page

---
+++ hocode-web/build/precache-manifest.b811d9bde2a1c62ed2397668c48e14b8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a530bd965ca539185a4f9cfba90049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be77cf4d085a65c906b1",
+    "url": "/static/css/2.106af11f.chunk.css"
+  },
+  {
+    "revision": "0023e50913d467424fa4",
+    "url": "/static/css/main.dbcfc3c0.chunk.css"
+  },
+  {
+    "revision": "be77cf4d085a65c906b1",
+    "url": "/static/js/2.7859f390.chunk.js"
+  },
+  {
+    "revision": "0023e50913d467424fa4",
+    "url": "/static/js/main.dcc6dd35.chunk.js"
+  },
+  {
+    "revision": "accb3400b7c5210f7cea",
+    "url": "/static/js/runtime-main.d5fd15bb.js"
+  },
+  {
+    "revision": "a6ac73da155f47394bca07e89ed1ed83",
+    "url": "/static/media/chuahoanthanh.a6ac73da.svg"
+  },
+  {
+    "revision": "eb842a5578ae8d082a51b3b155a37d73",
+    "url": "/static/media/giphy.eb842a55.gif"
+  },
+  {
+    "revision": "c18c22edd75e4c6442af5752b56d647e",
+    "url": "/static/media/hoanthanh.c18c22ed.svg"
+  },
+  {
+    "revision": "b54be03d3f1880ca9ab93c20f0f0472e",
+    "url": "/static/media/logo.b54be03d.png"
+  },
+  {
+    "revision": "f593928a2a034df77ef92b0fb15ca2d9",
+    "url": "/static/media/padlock-unlock.f593928a.svg"
+  },
+  {
+    "revision": "8b3ac6fe5634a957a45b796ba55c7615",
+    "url": "/static/media/play-button.8b3ac6fe.svg"
+  },
+  {
+    "revision": "316d47a9ff02ca1801f24fd7b4ba6771",
+    "url": "/static/media/user.316d47a9.svg"
+  }
+]);
