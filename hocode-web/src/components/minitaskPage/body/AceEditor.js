fec42537bf4e0ff86b359795b6520b88aca87924
Neun <duythien0912@gmail.com>
2019-11-27 23:15:31 +0700

    [Thien] color java code react-codemirror2

--- hocode-web/src/components/minitaskPage/body/AceEditor.js
+++ hocode-web/src/components/minitaskPage/body/AceEditor.js
@@ -1,48 +1,51 @@
 import React, { Component } from "react";
-import AceEditor from 'react-ace';
+import AceEditor from "react-ace";
 import "brace/mode/java";
 import "brace/snippets/java";
 import "brace/ext/language_tools";
 import "brace/theme/github";
+import "ace-builds/src-min-noconflict/ext-searchbox";
+import "ace-builds/src-min-noconflict/ext-language_tools";
+
+import "ace-builds/src-min-noconflict/mode-java";
+import "ace-builds/src-min-noconflict/snippets/java";
 
 
 class CodeEditor extends Component {
   render() {
-    return (
+    return ( 
       <AceEditor
-      style={{
-        
-        position: 'absolute !important',
-        top: '0 !important',
-        left: '0 !important',
-        bottom: '0 !important',
-        right: '0 !important',
-        
-        
+        style={{
+          position: "absolute !important",
+          top: "0 !important",
+          left: "0 !important",
+          bottom: "0 !important",
+          right: "0 !important"
         }}
-      editorProps={{
-      $blockScrolling: Infinity
-      }}
-      placeholder="Placeholder Text"
-      mode="java"
-      theme="github"
-      name="blah2"
-      onLoad={this.onLoad}
-      onChange={this.onChange}
-      fontSize={14}
-      showPrintMargin={true}
-      showGutter={true}
-      highlightActiveLine={true}
-      value={`function onLoad(editor) {
+        editorProps={{
+          $blockScrolling: Infinity
+        }}
+        placeholder="Placeholder Text"
+        mode="java"
+        theme="github"
+        name="blah2"
+        onLoad={this.onLoad}
+        onChange={this.onChange} 
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={`function onLoad(editor) {
       console.log("i've loaded");
     }`}
-      setOptions={{
-      enableBasicAutocompletion: true,
-      enableLiveAutocompletion: true,
-      enableSnippets: true,
-      showLineNumbers: true,
-      tabSize: 2,
-      }}/>
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: true,
+          showLineNumbers: true,
+          tabSize: 2
+        }}
+      />
     );
   }
 }
--- hocode-web/src/components/minitaskPage/body/CodeEditor.js
+++ hocode-web/src/components/minitaskPage/body/CodeEditor.js
@@ -1,39 +1,29 @@
-
 import React, { Component } from "react";
 import { Controlled as CodeMirror } from "react-codemirror2";
-import './codeeditor.css';
+import "./codeeditor.css";
 import "codemirror/addon/hint/show-hint.css";
 
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/theme/neat.css";
 
-
-import "codemirror/mode/clike/clike.js"
+import "codemirror/mode/clike/clike.js";
 //import "codemirror/mode/javascript/javascript.js"
-import "codemirror/addon/edit/closebrackets.js"
-import "codemirror/addon/edit/closetag.js"
-import "codemirror/addon/edit/matchbrackets.js"
-import "codemirror/addon/hint/show-hint.js"
+import "codemirror/addon/edit/closebrackets.js";
+import "codemirror/addon/edit/closetag.js";
+import "codemirror/addon/edit/matchbrackets.js";
+import "codemirror/addon/hint/show-hint.js";
 //import "codemirror/addon/hint/javascript-hint.js"
-import "codemirror/addon/hint/css-hint.js"
-import "codemirror/addon/hint/anyword-hint.js"
-
-
-
-
-
+import "codemirror/addon/hint/css-hint.js";
+import "codemirror/addon/hint/anyword-hint.js";
 
 class CodeEditor extends Component {
- 
   render() {
     return (
       <CodeMirror
-      value={
-        this.props.userCode
-      }
+        value={this.props.userCode}
         options={{
-         // mode: "javascript",
+          mode: "text/x-java", 
           theme: "material",
           lineNumbers: true,
           highlightSelectionMatches: true,
@@ -42,22 +32,20 @@ class CodeEditor extends Component {
           lineWrapping: true,
           dragDrop: false,
           autoCloseBrackets: true,
-          spellcheck:true,
-          autocorrect:true,
-          extraKeys:{"Ctrl-Space":"autocomplete"},
-          
-          
+          spellcheck: true,
+          autocorrect: true,
+          extraKeys: { "Ctrl-Space": "autocomplete" }
         }}
-        MarkText={{from:{line:0,ch:1}, to:{line:3,ch:1}, css: "font-size:5px"}}
+        MarkText={{
+          from: { line: 0, ch: 1 },
+          to: { line: 3, ch: 1 },
+          css: "font-size:5px"
+        }}
         onBeforeChange={(editor, data, value) => {
           this.props.updateUserCode(value); // update state usercode in component minitaskpage
         }}
-        onChange={(editor, data, value) => {
-         
-        }}
+        onChange={(editor, data, value) => {}}
       />
-      
-
     );
   }
 }
