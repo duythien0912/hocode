1d8556d5e86c075931909cc8097d6695cc134649
Neun <duythien0912@gmail.com>
2019-12-21 11:37:12 +0700

    [Thien] deploy to vps

--- hocode-server/web/service-worker.js
+++ hocode-server/web/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.08a9a9b00e46d8b79b3326802d993c60.js"
+  "/precache-manifest.b3df0c65ca8718176d9203005190125a.js"
 );
 
 self.addEventListener('message', (event) => {
